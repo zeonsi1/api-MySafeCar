e6f3218d696837d94a80f8752b9071fb310ab8d1
Zeonsi <tomasgarrido0212@gmial.com>
2024-07-24 15:06:07 -0400

    RE-fix order

--- src/controller/index.controller.js
+++ src/controller/index.controller.js
@@ -8,7 +8,7 @@ const pool = new Pool({
 });
 
 const getEstacionamiento = async(req, res) => {
-    const response = await pool.query('SELECT * FROM estacionamiento ORDER BY id_estacionamiento');
+    const response = await pool.query('SELECT * FROM estacionamiento ORDER BY CAST(substring(id_estacionamiento FROM \'[0-9]+\') AS INTEGER)');
     res.status(200).json(response.rows);
 };
 
